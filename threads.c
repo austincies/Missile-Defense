1fc1f0e890818c2717990b32b726689e4f0be4d7
Austin  Cieslinski <axc5447@cs.rit.edu>
2018-12-02 23:12:32 -0500

    baseline commit for threads.c

---
+++ threads.c
@@ -0,0 +1,12 @@
+#include <curses.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char *argv[] ) {
+    if( argc != 2 ) {
+            fprintf( stderr, "Usage: threads config-file\n" );
+            exit( EXIT_FAILURE );
+    }
+
+}
